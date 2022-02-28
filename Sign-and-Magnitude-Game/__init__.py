1485169416a72f22dbbf02e1f827628c57e8106a
JoeGreaves17 <74414406+JoeGreaves17@users.noreply.github.com>
2022-02-28 14:03:24 +0000

    Update __init__.py
    The description is in the docstring

--- Sign-and-Magnitude-Game/__init__.py
+++ Sign-and-Magnitude-Game/__init__.py
@@ -1,3 +1,2 @@
-""" For Flit: """
+"""Test yourself on converting denary numbers to sign and magnitude binary"""
 __version__ = "1.0.1"
-__description__ = "Test yourself on converting denary numbers to sign and magnitude binary"
